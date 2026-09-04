86009a4ac99a9a41f1ade8abdbdef17b8232e6bf
SizukuMurakawa <2025014467@metro.ed.jp>
2026-09-05 01:12:36 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -38,7 +38,6 @@ const booths = [
   { id:"ブレイクダンス部", name:"ブレイクダンス部", title:"BREAK DANCE SHOW", category:"公演", place:"体育館", building:"体育館棟", group:"部活動", wait:0, image:"" },
 
   { id:"有志演劇", name:"有志演劇", title:"しらかし祭公演『ケチャップ・オブ・ザ・テッド』", category:"演劇", place:"剣道場", building:"体育館棟", group:"有志", wait:0, image:"" },
-  { id:"有志情報研究", name:"有志情報研究", title:"しらかし祭待ち時間表示アプリ『Azuma』", category:"その他", place:"PC教室", building:"校舎棟", group:"有志", wait:0, image:"" },
   { id:"有志書道・競技かるた", name:"有志書道・競技かるた", title:"東書展", category:"展示", place:"3年8組", building:"校舎棟", group:"有志", wait:0, image:"" },
   { id:"ますらを中庭公演", name:"ますらを", title:"有志男声合唱団ますらを 中庭公演", category:"公演", place:"中庭", building:"校舎棟", group:"有志", wait:0, image:"" },
   { id:"ますらを黙示録", name:"ますらを", title:"合唱黙示録マスラヲ", category:"演劇", place:"剣道場", building:"体育館棟", group:"有志", wait:0, image:"" },
